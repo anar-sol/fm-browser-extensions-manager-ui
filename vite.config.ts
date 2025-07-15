1505b2eb9cbedfdeb65a01ee61784314b4c79145
Anar <anar.sol.dev@gmail.com>
2025-07-16 00:58:36 +0100

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss(),],
   base: "/fm-browser-extensions-manager-ui/",
+  plugins: [react(), svgr(), tailwindcss(),],
 })
